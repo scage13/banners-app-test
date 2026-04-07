ec4fe83f8becea92b289da977956174f4a1c11eb
Andrii Kravchuk <avkravchuk13@gmail.com>
2026-04-08 00:03:57 +0300

    Remove redundant `provideAnimations` provider from app config

--- apps/front-banners-app/src/app/app.config.ts
+++ apps/front-banners-app/src/app/app.config.ts
@@ -4,15 +4,13 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { appRoutes } from './app.routes';
 import { baseUrlInterceptor } from './core/interceptors/base-url.interceptor';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes),
     provideHttpClient(withInterceptors([baseUrlInterceptor])),
-    provideAnimations(),
   ],
 };
